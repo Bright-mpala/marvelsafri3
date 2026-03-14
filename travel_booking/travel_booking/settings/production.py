b773e2f34d29fd12969b0dc853ff6cf5b6c859e2
Bright-mpala <brightjustinmpala@gmail.com>
2026-03-14 12:12:11 +0200

    other updates

--- travel_booking/travel_booking/settings/production.py
+++ travel_booking/travel_booking/settings/production.py
@@ -7,7 +7,7 @@ import logging
 
 ENVIRONMENT = 'production'
 DEBUG = False
-SECRET_KEY = config('gy+4y(&(#2%!b02actgnm2wmjz06=uu=cfz_))rzbipqsdi$xi')  # Must be set in environment
+SECRET_KEY = config('SECRET_KEY')  # Must be set in environment
 
 ALLOWED_HOSTS = [
     'marvelsafri3-production.up.railway.app',
